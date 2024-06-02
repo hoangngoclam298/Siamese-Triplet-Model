b6c1fcf6ed4de17bf54e06a3b8089e62e59c1b10
Hoàng Ngọc Lâm <pm2_intern_02@example1.com>
2024-06-02 10:07:03 +0700

    update 2/6

--- datasets.py
+++ datasets.py
@@ -1,57 +1,119 @@
 import numpy as np
 from PIL import Image
-
+import os
 from torch.utils.data import Dataset
 from torch.utils.data.sampler import BatchSampler
 
 
-class SiameseMNIST(Dataset):
-    """
-    Train: For each sample creates randomly a positive or a negative pair
-    Test: Creates fixed pairs for testing
-    """
+class RandomResizedCropRect(object):
+    def __init__(self, size, scale=(0.8, 1.0)):
+        self.size = size
+        self.scale = scale
 
-    def __init__(self, mnist_dataset):
-        self.mnist_dataset = mnist_dataset
+    def __call__(self, img):
+        # Lấy kích thước ảnh
+        img_width, img_height = img.size
 
-        self.train = self.mnist_dataset.train
-        self.transform = self.mnist_dataset.transform
+        # Tính toán kích thước và tọa độ cho crop
+        crop_width = int(img_width * np.random.uniform(*self.scale))
+        crop_height = int(img_height * np.random.uniform(*self.scale))
+        crop_left = np.random.randint(0, img_width - crop_width + 1)
+        crop_top = np.random.randint(0, img_height - crop_height + 1)
+        crop_right = crop_left + crop_width
+        crop_bottom = crop_top + crop_height
 
-        if self.train:
-            self.train_labels = self.mnist_dataset.train_labels
-            self.train_data = self.mnist_dataset.train_data
-            self.labels_set = set(self.train_labels.numpy())
-            self.label_to_indices = {label: np.where(self.train_labels.numpy() == label)[0]
-                                     for label in self.labels_set}
-        else:
-            # generate fixed pairs for testing
-            self.test_labels = self.mnist_dataset.test_labels
-            self.test_data = self.mnist_dataset.test_data
-            self.labels_set = set(self.test_labels.numpy())
-            self.label_to_indices = {label: np.where(self.test_labels.numpy() == label)[0]
-                                     for label in self.labels_set}
+        # Crop ảnh
+        img = img.crop((crop_left, crop_top, crop_right, crop_bottom))
 
+        # Resize ảnh về kích thước mong muốn
+        img = img.resize(self.size, resample=Image.BILINEAR)
+
+        return img
+
+class GaussianNoise(object):
+    def __init__(self, mean=0, std=0.1):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, img):
+        img_np = np.array(img, dtype=np.float32)
+        row, col, ch = img_np.shape
+
+        # Tính toán kích thước của vùng ảnh sẽ nhận nhiễu
+        min_size = int(row * col * 0.1)
+        max_size = int(row * col * 0.25)
+        area_size = np.random.randint(min_size, max_size)
+
+        # Tạo ma trận mask để chỉ định vùng của ảnh sẽ nhận nhiễu
+        mask = np.zeros((row, col), dtype=np.uint8)
+        x = np.random.randint(0, col)
+        y = np.random.randint(0, row)
+        x_end = min(x + int(np.sqrt(area_size)), col)
+        y_end = min(y + int(np.sqrt(area_size)), row)
+        mask[y:y_end, x:x_end] = 1
+
+        # Tạo ma trận nhiễu Gaussian
+        std = np.random.uniform(0, 0.1)
+        gauss = np.random.normal(self.mean, std, (row, col, ch))
+
+        # Áp dụng nhiễu vào phần của ảnh được chỉ định bởi mask
+        noisy_img = img_np + gauss * mask[:, :, np.newaxis]
+        noisy_img = np.clip(noisy_img, 0, 255).astype(np.uint8)
+
+        return Image.fromarray(noisy_img)
+
+def fixed_image_standardization(image_tensor):
+    processed_tensor = (image_tensor - 127.5) / 128.0
+    return processed_tensor
+
+class CustomDataset(Dataset):
+    def __init__(self, data_dir, transform=None, train=True):
+        self.data_dir = data_dir
+        self.transform = transform
+        self.train = train
+
+        self.image_paths = []
+        self.labels = []
+
+        for label in os.listdir(data_dir):
+            label_dir = os.path.join(data_dir, label)
+            if os.path.isdir(label_dir):
+                for image_name in os.listdir(label_dir):
+                    image_path = os.path.join(label_dir, image_name)
+                    self.image_paths.append(image_path)
+                    self.labels.append(int(label))  # assuming labels are integers
+
+        self.labels = np.array(self.labels)
+        self.labels_set = set(self.labels)
+        self.label_to_indices = {label: np.where(self.labels == label)[0]
+                                 for label in self.labels_set}
+
+        if not self.train:
             random_state = np.random.RandomState(29)
 
             positive_pairs = [[i,
-                               random_state.choice(self.label_to_indices[self.test_labels[i].item()]),
+                               random_state.choice(self.label_to_indices[self.labels[i]]),
                                1]
-                              for i in range(0, len(self.test_data), 2)]
+                              for i in range(0, len(self.image_paths), 2)]
 
             negative_pairs = [[i,
                                random_state.choice(self.label_to_indices[
                                                        np.random.choice(
-                                                           list(self.labels_set - set([self.test_labels[i].item()]))
+                                                           list(self.labels_set - set([self.labels[i]]))
                                                        )
                                                    ]),
                                0]
-                              for i in range(1, len(self.test_data), 2)]
+                              for i in range(1, len(self.image_paths), 2)]
             self.test_pairs = positive_pairs + negative_pairs
+
+    def __len__(self):
+        return len(self.image_paths)
 
     def __getitem__(self, index):
         if self.train:
             target = np.random.randint(0, 2)
-            img1, label1 = self.train_data[index], self.train_labels[index].item()
+            img1_path = self.image_paths[index]
+            label1 = self.labels[index]
             if target == 1:
                 siamese_index = index
                 while siamese_index == index:
@@ -59,125 +121,15 @@ class SiameseMNIST(Dataset):
             else:
                 siamese_label = np.random.choice(list(self.labels_set - set([label1])))
                 siamese_index = np.random.choice(self.label_to_indices[siamese_label])
-            img2 = self.train_data[siamese_index]
+            img2_path = self.image_paths[siamese_index]
         else:
-            img1 = self.test_data[self.test_pairs[index][0]]
-            img2 = self.test_data[self.test_pairs[index][1]]
+            img1_path = self.image_paths[self.test_pairs[index][0]]
+            img2_path = self.image_paths[self.test_pairs[index][1]]
             target = self.test_pairs[index][2]
 
-        img1 = Image.fromarray(img1.numpy(), mode='L')
-        img2 = Image.fromarray(img2.numpy(), mode='L')
+        img1 = Image.open(img1_path).convert('RGB')
+        img2 = Image.open(img2_path).convert('RGB')
         if self.transform is not None:
             img1 = self.transform(img1)
             img2 = self.transform(img2)
         return (img1, img2), target
-
-    def __len__(self):
-        return len(self.mnist_dataset)
-
-
-class TripletMNIST(Dataset):
-    """
-    Train: For each sample (anchor) randomly chooses a positive and negative samples
-    Test: Creates fixed triplets for testing
-    """
-
-    def __init__(self, mnist_dataset):
-        self.mnist_dataset = mnist_dataset
-        self.train = self.mnist_dataset.train
-        self.transform = self.mnist_dataset.transform
-
-        if self.train:
-            self.train_labels = self.mnist_dataset.train_labels
-            self.train_data = self.mnist_dataset.train_data
-            self.labels_set = set(self.train_labels.numpy())
-            self.label_to_indices = {label: np.where(self.train_labels.numpy() == label)[0]
-                                     for label in self.labels_set}
-
-        else:
-            self.test_labels = self.mnist_dataset.test_labels
-            self.test_data = self.mnist_dataset.test_data
-            # generate fixed triplets for testing
-            self.labels_set = set(self.test_labels.numpy())
-            self.label_to_indices = {label: np.where(self.test_labels.numpy() == label)[0]
-                                     for label in self.labels_set}
-
-            random_state = np.random.RandomState(29)
-
-            triplets = [[i,
-                         random_state.choice(self.label_to_indices[self.test_labels[i].item()]),
-                         random_state.choice(self.label_to_indices[
-                                                 np.random.choice(
-                                                     list(self.labels_set - set([self.test_labels[i].item()]))
-                                                 )
-                                             ])
-                         ]
-                        for i in range(len(self.test_data))]
-            self.test_triplets = triplets
-
-    def __getitem__(self, index):
-        if self.train:
-            img1, label1 = self.train_data[index], self.train_labels[index].item()
-            positive_index = index
-            while positive_index == index:
-                positive_index = np.random.choice(self.label_to_indices[label1])
-            negative_label = np.random.choice(list(self.labels_set - set([label1])))
-            negative_index = np.random.choice(self.label_to_indices[negative_label])
-            img2 = self.train_data[positive_index]
-            img3 = self.train_data[negative_index]
-        else:
-            img1 = self.test_data[self.test_triplets[index][0]]
-            img2 = self.test_data[self.test_triplets[index][1]]
-            img3 = self.test_data[self.test_triplets[index][2]]
-
-        img1 = Image.fromarray(img1.numpy(), mode='L')
-        img2 = Image.fromarray(img2.numpy(), mode='L')
-        img3 = Image.fromarray(img3.numpy(), mode='L')
-        if self.transform is not None:
-            img1 = self.transform(img1)
-            img2 = self.transform(img2)
-            img3 = self.transform(img3)
-        return (img1, img2, img3), []
-
-    def __len__(self):
-        return len(self.mnist_dataset)
-
-
-class BalancedBatchSampler(BatchSampler):
-    """
-    BatchSampler - from a MNIST-like dataset, samples n_classes and within these classes samples n_samples.
-    Returns batches of size n_classes * n_samples
-    """
-
-    def __init__(self, labels, n_classes, n_samples):
-        self.labels = labels
-        self.labels_set = list(set(self.labels.numpy()))
-        self.label_to_indices = {label: np.where(self.labels.numpy() == label)[0]
-                                 for label in self.labels_set}
-        for l in self.labels_set:
-            np.random.shuffle(self.label_to_indices[l])
-        self.used_label_indices_count = {label: 0 for label in self.labels_set}
-        self.count = 0
-        self.n_classes = n_classes
-        self.n_samples = n_samples
-        self.n_dataset = len(self.labels)
-        self.batch_size = self.n_samples * self.n_classes
-
-    def __iter__(self):
-        self.count = 0
-        while self.count + self.batch_size < self.n_dataset:
-            classes = np.random.choice(self.labels_set, self.n_classes, replace=False)
-            indices = []
-            for class_ in classes:
-                indices.extend(self.label_to_indices[class_][
-                               self.used_label_indices_count[class_]:self.used_label_indices_count[
-                                                                         class_] + self.n_samples])
-                self.used_label_indices_count[class_] += self.n_samples
-                if self.used_label_indices_count[class_] + self.n_samples > len(self.label_to_indices[class_]):
-                    np.random.shuffle(self.label_to_indices[class_])
-                    self.used_label_indices_count[class_] = 0
-            yield indices
-            self.count += self.n_classes * self.n_samples
-
-    def __len__(self):
-        return self.n_dataset // self.batch_size
