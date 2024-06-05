cab6c09766f88be5ffc5af01439e9deed4468d7a
Hoàng Ngọc Lâm <pm2_intern_02@example1.com>
2024-06-05 22:45:13 +0700

    add Concat dataset, network

--- datasets.py
+++ datasets.py
@@ -197,3 +197,145 @@ class TripletDataset(Dataset):
             img3 = self.transform(img3)
 
         return (img1, img2, img3), []
+
+class SiameseDatasetConcat(Dataset):
+    def __init__(self, data_dir1, data_dir2, transform=None, train=True):
+        self.data_dir1 = data_dir1
+        self.data_dir2 = data_dir2
+        self.transform = transform
+        self.train = train
+
+        self.image_paths = []
+        self.labels = []
+
+        for label in os.listdir(data_dir1):
+            label_dir1 = os.path.join(data_dir1, label)
+            label_dir2 = os.path.join(data_dir2, label)
+            if os.path.isdir(label_dir1):
+                for image_name in os.listdir(label_dir1):
+                    image_path1 = os.path.join(label_dir1, image_name)
+                    image_path2 = os.path.join(label_dir2, image_name)
+                    self.image_paths.append((image_path1, image_path2))
+                    self.labels.append(int(label))  # assuming labels are integers
+
+        self.labels = np.array(self.labels)
+        self.labels_set = set(self.labels)
+        self.label_to_indices = {label: np.where(self.labels == label)[0]
+                                 for label in self.labels_set}
+
+        if not self.train:
+            random_state = np.random.RandomState(29)
+
+            positive_pairs = [[i,
+                               random_state.choice(self.label_to_indices[self.labels[i]]),
+                               1]
+                              for i in range(0, len(self.image_paths), 2)]
+
+            negative_pairs = [[i,
+                               random_state.choice(self.label_to_indices[
+                                                       np.random.choice(
+                                                           list(self.labels_set - set([self.labels[i]]))
+                                                       )
+                                                   ]),
+                               0]
+                              for i in range(1, len(self.image_paths), 2)]
+            self.test_pairs = positive_pairs + negative_pairs
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, index):
+        if self.train:
+            target = np.random.randint(0, 2)
+            img1_path = self.image_paths[index]
+            label1 = self.labels[index]
+            if target == 1:
+                siamese_index = index
+                while siamese_index == index:
+                    siamese_index = np.random.choice(self.label_to_indices[label1])
+            else:
+                siamese_label = np.random.choice(list(self.labels_set - set([label1])))
+                siamese_index = np.random.choice(self.label_to_indices[siamese_label])
+            img2_path = self.image_paths[siamese_index]
+        else:
+            img1_path = self.image_paths[self.test_pairs[index][0]]
+            img2_path = self.image_paths[self.test_pairs[index][1]]
+            target = self.test_pairs[index][2]
+
+        img1 = Image.open(img1_path[0]).convert('RGB')
+        img2 = Image.open(img1_path[1]).convert('RGB')        
+        img3 = Image.open(img2_path[0]).convert('RGB')
+        img4 = Image.open(img2_path[1]).convert('RGB')
+        if self.transform is not None:
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+            img3 = self.transform(img3)
+            img4 = self.transform(img4)
+        return (img1, img2, img3, img4), target
+    
+    
+class TripletDataset(Dataset):
+    def __init__(self, data_dir1, data_dir2, transform=None, train=True):
+        self.data_dir1 = data_dir1
+        self.data_dir2 = data_dir2
+        self.transform = transform
+        self.train = train
+
+        self.image_paths = []
+        self.labels = []
+
+        for label in os.listdir(data_dir1):
+            label_dir1 = os.path.join(data_dir1, label)
+            label_dir2 = os.path.join(data_dir2, label)
+            if os.path.isdir(label_dir1):
+                for image_name in os.listdir(label_dir1):
+                    image_path1 = os.path.join(label_dir1, image_name)
+                    image_path2 = os.path.join(label_dir2, image_name)
+                    self.image_paths.append((image_path1, image_path2))
+                    self.labels.append(int(label))  # assuming labels are integers
+
+        if not self.train:
+            random_state = np.random.RandomState(29)
+
+            self.test_triplets = [[i,
+                                   random_state.choice(self.label_to_indices[self.labels[i]]),
+                                   random_state.choice(self.label_to_indices[
+                                       np.random.choice(list(self.labels_set - set([self.labels[i]])))
+                                   ])
+                                  ]
+                                 for i in range(len(self.image_paths))]
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, index):
+        if self.train:
+            img1_path = self.image_paths[index]
+            label1 = self.labels[index]
+            positive_index = index
+            while positive_index == index:
+                positive_index = np.random.choice(self.label_to_indices[label1])
+            negative_label = np.random.choice(list(self.labels_set - set([label1])))
+            negative_index = np.random.choice(self.label_to_indices[negative_label])
+            img2_path = self.image_paths[positive_index]
+            img3_path = self.image_paths[negative_index]
+        else:
+            img1_path = self.image_paths[self.test_triplets[index][0]]
+            img2_path = self.image_paths[self.test_triplets[index][1]]
+            img3_path = self.image_paths[self.test_triplets[index][2]]
+
+        img1 = Image.open(img1_path[0]).convert('RGB')
+        img2 = Image.open(img1_path[1]).convert('RGB')
+        img3 = Image.open(img2_path[0]).convert('RGB')
+        img4 = Image.open(img2_path[1]).convert('RGB')
+        img5 = Image.open(img3_path[0]).convert('RGB')
+        img6 = Image.open(img3_path[1]).convert('RGB')
+
+        if self.transform is not None:
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+            img3 = self.transform(img3)
+            img4 = self.transform(img4)
+            img5 = self.transform(img5)
+            img6 = self.transform(img6)
+        return (img1, img2, img3, img4, img5, img6), []
--- networks.py
+++ networks.py
@@ -266,6 +266,149 @@ class EmbeddingNet(nn.Module):
     def get_embedding(self, x):
         return self.forward(x)
 
+class InceptionResnetV1_feature(nn.Module):
+    """Inception Resnet V1 model with optional loading of pretrained weights.
+
+    Model parameters can be loaded based on pretraining on the VGGFace2 or CASIA-Webface
+    datasets. Pretrained state_dicts are automatically downloaded on model instantiation if
+    requested and cached in the torch cache. Subsequent instantiations use the cache rather than
+    redownloading.
+
+    Keyword Arguments:
+        pretrained {str} -- Optional pretraining dataset. Either 'vggface2' or 'casia-webface'.
+            (default: {None})
+        classify {bool} -- Whether the model should output classification probabilities or feature
+            embeddings. (default: {False})
+        num_classes {int} -- Number of output classes. If 'pretrained' is set and num_classes not
+            equal to that used for the pretrained model, the final linear layer will be randomly
+            initialized. (default: {None})
+        dropout_prob {float} -- Dropout probability. (default: {0.6})
+    """
+    def __init__(self, dropout_prob=0.6):
+        super().__init__()
+        # Define layers
+        self.conv2d_1a = BasicConv2d(3, 32, kernel_size=3, stride=2)
+        self.conv2d_2a = BasicConv2d(32, 32, kernel_size=3, stride=1)
+        self.conv2d_2b = BasicConv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.maxpool_3a = nn.MaxPool2d(3, stride=2)
+        self.conv2d_3b = BasicConv2d(64, 80, kernel_size=1, stride=1)
+        self.conv2d_4a = BasicConv2d(80, 192, kernel_size=3, stride=1)
+        self.conv2d_4b = BasicConv2d(192, 256, kernel_size=3, stride=2)
+        self.repeat_1 = nn.Sequential(
+            Block35(scale=0.17),
+            Block35(scale=0.17),
+            Block35(scale=0.17),
+            Block35(scale=0.17),
+            Block35(scale=0.17),
+        )
+        self.mixed_6a = Mixed_6a()
+        self.repeat_2 = nn.Sequential(
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+            Block17(scale=0.10),
+        )
+        self.mixed_7a = Mixed_7a()
+        self.repeat_3 = nn.Sequential(
+            Block8(scale=0.20),
+            Block8(scale=0.20),
+            Block8(scale=0.20),
+            Block8(scale=0.20),
+            Block8(scale=0.20),
+        )
+        self.block8 = Block8(noReLU=True)
+        self.avgpool_1a = nn.AdaptiveAvgPool2d(1)
+        self.dropout = nn.Dropout(dropout_prob)
+        self.last_linear = nn.Linear(1792, 256, bias=False)
+
+    def forward(self, x):
+        """Calculate embeddings or logits given a batch of input image tensors.
+
+        Arguments:
+            x {torch.tensor} -- Batch of image tensors representing faces.
+
+        Returns:
+            torch.tensor -- Batch of embedding vectors or multinomial logits.
+        """
+        x = self.conv2d_1a(x)
+        x = self.conv2d_2a(x)
+        x = self.conv2d_2b(x)
+        x = self.maxpool_3a(x)
+        x = self.conv2d_3b(x)
+        x = self.conv2d_4a(x)
+        x = self.conv2d_4b(x)
+        x = self.repeat_1(x)
+        x = self.mixed_6a(x)
+        x = self.repeat_2(x)
+        x = self.mixed_7a(x)
+        x = self.repeat_3(x)
+        x = self.block8(x)
+        x = self.avgpool_1a(x)
+        x = self.dropout(x)
+        x = self.last_linear(x.view(x.shape[0], -1))
+        return x
+
+class EmbeddingNet_Concat(nn.Module):
+    def __init__(self, num_classes=1000):
+        super(EmbeddingNet_Concat, self).__init__()
+        # Load pre-trained ResNet models
+        self.resnet1 = InceptionResnetV1_feature()
+        self.resnet2 = InceptionResnetV1_feature()
+        self.last_bn = nn.BatchNorm1d(512, eps=0.001, momentum=0.1, affine=True, track_running_stats=True)
+        self.logits = nn.Linear(512, num_classes, bias=True)
+        
+    def forward(self, x1, x2):
+        # Forward pass for the first image
+        x1 = self.resnet1(x1)
+        x1 = torch.flatten(x1, 1)
+        
+        x2 = self.resnet2(x2)
+        x2 = torch.flatten(x2, 1)        
+        # Concatenate the outputs
+        x = torch.cat((x1, x2), dim=1)
+        
+        # Apply batch normalization
+        x = self.last_bn(x)
+        
+        # Final logits
+        x = self.logits(x)
+        
+        return x
+    
+class SiameseNet_Concat(nn.Module):
+    def __init__(self, embedding_net):
+        super(SiameseNet_Concat, self).__init__()
+        self.embedding_net = embedding_net
+
+    def forward(self, x1, x2, x3, x4):
+        output1 = self.embedding_net(x1, x2)
+        output2 = self.embedding_net(x3, x4)
+        return output1, output2
+
+    def get_embedding(self, x1, x2):
+        return self.embedding_net(x1, x2)
+
+
+class TripletNet_Concat(nn.Module):
+    def __init__(self, embedding_net):
+        super(TripletNet_Concat, self).__init__()
+        self.embedding_net = embedding_net
+
+    def forward(self, x1, x2, x3, x4, x5, x6):
+        output1 = self.embedding_net(x1, x2)
+        output2 = self.embedding_net(x3, x4)
+        output3 = self.embedding_net(x5, x6)
+        return output1, output2, output3
+
+    def get_embedding(self, x1, x2):
+        return self.embedding_net(x1, x2)
+    
 class EmbeddingNetL2(EmbeddingNet):
     def __init__(self):
         super(EmbeddingNetL2, self).__init__()
